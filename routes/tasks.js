41f693709a69dbe1ec085da7648e323dac9684e2
syedanusrat007 <syedanusrat007.com>
2019-01-24 18:36:59 +0600

    Added my project

---
+++ routes/tasks.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://tonni:tonni012@ds163984.mlab.com:63984/to_do_list', ['tasks']);
+//var db = mongojs('mongodb://admin:123ali@ds257640.mlab.com:57640/creating-node-api', ['tasks']);
+// We are adding Docker-compose.yaml file and will create a underlying layer of docker with mongo so no need to connect URL of MongoDB
+//var db = mongojs('mongodb://mongo:27017', ['tasks']);
+
+
+
+// Get All Tasks
+router.get('/tasks', function(req, res, next){
+    db.tasks.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks);
+    });
+});
+
+// Get Single Task
+router.get('/task/:id', function(req, res, next){
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
+});
+
+//Save Task
+router.post('/task', function(req, res, next){
+    var task = req.body;
+    if(!task.title || !(task.isDone + '')){
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    } else {
+        db.tasks.save(task, function(err, task){
+            if(err){
+                res.send(err);
+            }
+            res.json(task);
+        });
+    }
+});
+
+// Delete Task
+router.delete('/task/:id', function(req, res, next){
+    db.tasks.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
+});
+
+// Update Task
+router.put('/task/:id', function(req, res, next){
+    var task = req.body;
+    var updTask = {};
+    
+    if(task.isDone){
+        updTask.isDone = task.isDone;
+    }
+    
+    if(task.title){
+        updTask.title = task.title;
+    }
+    
+    if(!updTask){
+        res.status(400);
+        res.json({
+            "error":"Bad Data"
+        });
+    } else {
+        db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updTask, {}, function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
+    }
+});
+
+module.exports = router;
